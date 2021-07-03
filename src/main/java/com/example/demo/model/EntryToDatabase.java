43232857cf2569a8b9d6cb9bc92eaf524f8da3ea
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-03 15:00:27 +0200

    maded entrace to db

---
+++ src/main/java/com/example/demo/model/EntryToDatabase.java
@@ -0,0 +1,82 @@
+package com.example.demo.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class DatabaseEntrace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "Number ID", required = true, dataType = "Long")
+    private Long Id;
+
+    @ApiModelProperty(notes = "Currency name", required = true, dataType = "String")
+    private String currency;
+
+    @ApiModelProperty(notes = "Days number", required = true, dataType = "Integer")
+    private Integer daysNumber;
+
+    @ApiModelProperty(notes = "Average prise from number of days", required = true, dataType = "Double")
+    private Double avgPrices;
+
+    @ApiModelProperty(notes = "Date + hours when avg price was taken", required = true, dataType = "String")
+    private final String createDate = createDate();
+
+    public String createDate(){
+        Date d = Calendar.getInstance().getTime();
+        DateFormat df = new SimpleDateFormat("yyyy.mm.dd HH");
+        return df.format(d);
+    }
+    
+    public DatabaseEntrace(String currency, Integer daysNumber, Double avgPrices) {
+        this.currency = currency;
+        this.daysNumber = daysNumber;
+        this.avgPrices = avgPrices;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Integer getDaysNumber() {
+        return daysNumber;
+    }
+
+    public void setDaysNumber(Integer daysNumber) {
+        this.daysNumber = daysNumber;
+    }
+
+    public Double getAvgPrices() {
+        return avgPrices;
+    }
+
+    public void setAvgPrices(Double avgPrices) {
+        this.avgPrices = avgPrices;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+}
