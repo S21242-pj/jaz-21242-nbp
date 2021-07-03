90f10ae1e62ea9951432d40b73845c7a3ae911b1
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-03 14:14:55 +0200

    fixing errors

--- src/main/java/com/example/demo/controller/NbpRestController.java
+++ src/main/java/com/example/demo/controller/NbpRestController.java
@@ -2,13 +2,11 @@ package com.example.demo.controller;
 
 
 import com.example.demo.service.NbpService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/bank")
 public class NbpRestController {
 
     private final NbpService nbpService;
--- src/main/java/com/example/demo/model/Rate.java
+++ src/main/java/com/example/demo/model/Rate.java
@@ -5,4 +5,33 @@ public class Rate {
     private String effectiveDate;
     private double mid;
 
+    public Rate(String no, String effectiveDate, double mid) {
+        this.no = no;
+        this.effectiveDate = effectiveDate;
+        this.mid = mid;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public double getMid() {
+        return mid;
+    }
+
+    public void setMid(double mid) {
+        this.mid = mid;
+    }
 }
