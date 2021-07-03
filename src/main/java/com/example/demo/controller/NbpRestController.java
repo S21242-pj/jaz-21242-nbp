b1f5ffeb15548d8bdfae32682d22e45ac7ab428c
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-03 14:06:26 +0200

    added controller

--- src/main/java/com/example/demo/controller/NbpRestController.java
+++ src/main/java/com/example/demo/controller/NbpRestController.java
@@ -7,4 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/test")
+public class NbpRestController {
 
+    private final NbpService nbpService;
+
+    public NbpRestController(NbpService nbpService) {
+        this.nbpService = nbpService;
+    }
+
+
+
+}
