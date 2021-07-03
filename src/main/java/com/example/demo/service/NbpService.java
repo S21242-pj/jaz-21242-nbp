8becc965492cbb1a2f4a5c8bdbb75bb22be97fc4
s21242-PJ <s21242@pjwstk.edu.pl>
2021-07-03 15:45:15 +0200

    asdsad

--- src/main/java/com/example/demo/service/NbpService.java
+++ src/main/java/com/example/demo/service/NbpService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.model.DatabaseEntrace;
 import com.example.demo.repository.NbpRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Currency;
 
 @Service
 public class NbpService {
@@ -14,5 +17,11 @@ public class NbpService {
         this.restTemplate = restTemplate;
         this.nbpRepository = nbpRepository;
     }
+
+    public DatabaseEntrace saveDataRangeAndCurrency(String currencyName, Integer firstDate, Integer lastDate, double avgCurrency){
+        DatabaseEntrace databaseEntrace = new DatabaseEntrace(currencyName, firstDate, lastDate, avgCurrency);
+        return curRep.save
+    }
+
 
 }
